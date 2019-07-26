5b70a265047e660ea9b2b5c32fa68071de02e6c1
Duy Quoc <qduy888@Duys-Mac.local>
2019-07-26 02:05:17 -0700

    + Update project skeleton

---
+++ ios/silkime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  silkime
+//
+//  Created by Duy Quoc on 7/26/19.
+//  Copyright © 2019 Duy Quoc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
